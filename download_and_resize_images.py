6cb0729556d0866a099da758717404e3a90f107d
IsmaFerror <isma53428@gmail.com>
2025-10-26 22:54:04 +0100

    little changes

--- download_and_resize_images.py
+++ download_and_resize_images.py
@@ -23,7 +23,6 @@ HEADERS = {"User-Agent": "resizer-downloader/1.0"}
 #example:
 images = {
     "Python_logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1024px-Python-logo-notext.svg.png",
-    
 }
 
 def fetch_image(url):
